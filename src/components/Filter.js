6c7742db6816e6e9e7a3cc5ec89b6b6b29c39d91
Nigouu <uggla.nicolas@gmail.com>
2021-09-28 12:52:28 +0300

    filters fixed

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-// import { filterChange } from '../reducers/filterReducer'
+import { filterChange } from '../reducers/filterReducer'
 
 const Filter = () => {
 
   const dispatch = useDispatch()
 
   const handleChange = (event) => {
-    dispatch({
-        type: 'SET_FILTER',
-        filter: event.target.value
-      })
+    dispatch(filterChange(event.target.value))
   }
 
   const style = {
